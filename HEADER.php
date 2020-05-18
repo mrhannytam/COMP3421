454d130ff77c4ccb481ab7f2ef0712720c1d29e8
ambertang802 <60965328+ambertang802@users.noreply.github.com>
2020-05-18 16:32:28 +0800

    3.5.0 change to 3.5.1

--- HEADER.php
+++ HEADER.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <!-- jQuery library -->
-<script src="./js/external/JQuery/jquery-3.5.0.js"></script>
+<script src="./js/external/JQuery/jquery-3.5.1.js"></script>
 
 <!-- Latest compiled JavaScript -->
 <script src="./js/external/Bootstrap/bootstrap.min.js"></script>
