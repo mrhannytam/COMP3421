4ae456438f9d02334a7d799426d6773b5be3c469
mrhannytam <mrhannytam@gmail.com>
2020-05-24 15:09:39 +0800

    test

--- servlet/getInventory.php
+++ servlet/getInventory.php
@@ -2,29 +2,31 @@
     $status = "fail";
     $data = "none";
 
-
     require_once('db.php');
     $con = DBConnection();
-    $sql = $con->prepare("SELECT * FROM inventory");
-    $sql->execute();
-    $sql->bind_result($inventory_id, $inventory_name, $inventory_image, $price);
-    $sql->store_result();
-    if($sql->num_rows > 0){
-        $data = array();
-        $i = 0;
-        while($sql->fetch()){
-            $data[$i] = array(
-                'inventory_id' => $inventory_id,
-                'inventory_name' => $inventory_name,
-                'inventory_image' => $inventory_image,
-                'price' => $price
-            );
-            $i++;
+    if(!$con->error){
+        $sql = $con->prepare("SELECT * FROM inventory");
+        $sql->execute();
+        $sql->bind_result($inventory_id, $inventory_name, $inventory_image, $price);
+        $sql->store_result();
+        if($sql->num_rows > 0){
+            $data = array();
+            $i = 0;
+            while($sql->fetch()){
+                $data[$i] = array(
+                    'inventory_id' => $inventory_id,
+                    'inventory_name' => $inventory_name,
+                    'inventory_image' => $inventory_image,
+                    'price' => $price
+                );
+                $i++;
+            }
+            $status = "success";
+        }else{
+            $data = "Shop haven't start yet";
         }
-        $status = "success";
-    }else{
-        $data = "Shop haven't start yet";
     }
+    
 
 
     echo json_encode(
