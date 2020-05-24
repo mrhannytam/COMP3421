0dd0aa4cbb617d05bd619f65ec40bc741131eb2f
mrhannytam <mrhannytam@gmail.com>
2020-05-24 14:55:51 +0800

    put on cloud

--- servlet/db.php
+++ servlet/db.php
@@ -1,9 +1,9 @@
 <?php
 function DBConnection(){
-    $host = "localhost";
-    $user = "root";
-    $password = "toor";
-    $database = "COMP3421";
+    $host = "den1.mysql2.gear.host";
+    $user = "petlato";
+    $password = "Vz4i~S-T3Ps4";
+    $database = "petlato";
     $con = mysqli_connect($host, $user, $password, $database);
     return $con;
 }
