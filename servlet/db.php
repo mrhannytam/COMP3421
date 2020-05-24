e3e7a652bbddfc45291f3604e1c29d9ee408c62c
mrhannytam <mrhannytam@gmail.com>
2020-05-24 15:26:13 +0800

    test

--- servlet/db.php
+++ servlet/db.php
@@ -1,13 +1,10 @@
 <?php
 function DBConnection(){
-    $host = "lolcahost";
+    $host = "den1.mysql2.gear.host";
     $user = "petlato";
     $password = "Vz4i~S-T3Ps4";
     $database = "petlato";
     $con = mysqli_connect($host, $user, $password, $database);
     return $con;
 }
-
-echo $con = DBConnection();
-echo 'hi';
 ?>
--- servlet/getInventory.php
+++ servlet/getInventory.php
@@ -5,35 +5,27 @@
     require_once('./db.php');
     $con = DBConnection();
 
-    if(!$con){
-        echo "Error: Unable to connect to MySQL." . PHP_EOL;
-        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-        exit;
-    }else{
-        $sql = $con->prepare("SELECT * FROM inventory");
-        $sql->execute();
-        $sql->bind_result($inventory_id, $inventory_name, $inventory_image, $price);
-        $sql->store_result();
-        if($sql->num_rows > 0){
-            $data = array();
-            $i = 0;
-            while($sql->fetch()){
-                $data[$i] = array(
-                    'inventory_id' => $inventory_id,
-                    'inventory_name' => $inventory_name,
-                    'inventory_image' => $inventory_image,
-                    'price' => $price
-                );
-                $i++;
-            }
-            $status = "success";
-        }else{
-            $data = "Shop haven't start yet";
+    $sql = $con->prepare("SELECT * FROM inventory");
+    $sql->execute();
+    $sql->bind_result($inventory_id, $inventory_name, $inventory_image, $price);
+    $sql->store_result();
+    if($sql->num_rows > 0){
+        $data = array();
+        $i = 0;
+        while($sql->fetch()){
+            $data[$i] = array(
+                'inventory_id' => $inventory_id,
+                'inventory_name' => $inventory_name,
+                'inventory_image' => $inventory_image,
+                'price' => $price
+            );
+            $i++;
         }
+        $status = "success";
+    }else{
+        $data = "Shop haven't start yet";
     }
-
-
+    
     echo json_encode(
         array(
             'status' => $status,
